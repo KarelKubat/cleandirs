8009e09e4be0377938f8c56998bd7349751976bf
Karel Kubat <karel@kubat.nl>
2023-11-23 16:55:36 +0100

    flag shortening added, custom version ID removed

--- cleandirs.go
+++ cleandirs.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"os"
 	"strings"
 	"syscall"
 	"time"
 
 	"github.com/KarelKubat/cleandirs/dir"
 	"github.com/KarelKubat/cleandirs/l"
+	"github.com/KarelKubat/flagnames"
 )
 
 var (
@@ -18,29 +17,18 @@ var (
 	pruneDirs   = flag.Bool("prune-dirs", true, "when true, empty directories under --dirs-to-clean are removed")
 	allFiles    = flag.Bool("all-files", false, "when true, remove also special files (pipes, fifo's etc.), default: regular files only")
 	dryRun      = flag.Bool("dry-run", true, "when true, suggested removals are shown but not actuated")
-	version     = flag.Bool("version", false, "show version ID, then stop")
 
-	usageInfo = fmt.Sprintf(`
-
-This is cleandirs V%s, a tiny utility to remove stale files in temporary directories.
+	usageInfo = `
+This is cleandirs, a tiny utility to remove stale files in temporary directories.
 Try cleandirs -help for a listing of available flags.	
-	`, versionID)
-)
-
-const (
-	versionID = "1.01"
+`
 )
 
 func main() {
-
+	flagnames.Patch()
 	flag.Parse()
 	if len(flag.Args()) != 0 {
 		l.Printf(l.FATAL, usageInfo)
-	}
-
-	if *version {
-		fmt.Println(versionID)
-		os.Exit(1)
 	}
 
 	cutoff := time.Now().Add(-*ttl)
@@ -54,6 +42,7 @@ func cleanFilesIn(d string, cutoff time.Time) {
 	entries, err := dir.List(d)
 	if err != nil {
 		l.Printf(l.WARN, "%q: failed to list entries: %v\n", d, err)
+		return
 	}
 	var subdirs []string
 	for _, e := range entries {
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/KarelKubat/cleandirs
 
 go 1.17
+
+require github.com/KarelKubat/flagnames v1.0.2 // indirect
